1fef51ae5074e48ebd78e010ef4e7140230c9280
yogenderashell <yogender.sharma23@lpu.in>
2024-02-17 18:10:31 +0530

    add user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const VideoShema = new Schema(
+  {
+    videofile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    ispublic: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+VideoShema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", VideoShema);
